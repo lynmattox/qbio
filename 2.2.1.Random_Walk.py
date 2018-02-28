0c775ef496ae0a590e0a3f167d9e539a08810f9d
lynmattox <lynmattox@users.noreply.github.com>
2018-02-27 22:23:49 -0500

    Finished pythoning first function, but errors

--- 2.2.1.Random_Walk.py
+++ 2.2.1.Random_Walk.py
@@ -1,4 +1,6 @@
 import random as r
+import numpy as n 
+from operator import add 
 
 def RandomWalk(x0, y0, t0, L, tau, s, runs, seed):
     # Where x_0, y_0 are the initial spatial coordinates
@@ -18,62 +20,61 @@ def RandomWalk(x0, y0, t0, L, tau, s, runs, seed):
     else : 
         raise Exception("This is only one step!")
         
-    #initiallizes coordinate vectors
-    runsbysteps <- array(dim=c(steps,3,runs))
-  
-  runsbysteps[1,,] <- c(x_0,y_0,t_0)
-  
-  #Runs any number of simmulations 
-  for (i in 1:runs)
-  {
-    #Computes position at each time step
-    for (j in 2:steps)
-    {
-      #theta is the direction of movement (from a uniform random distribution)
-      #0 is East
-      theta <- runif(1, 0, 2*pi)
-      
-      #displacement in this step
-      dx <- L * cos(theta)
-      dy <- L * sin(theta)
+    #initiallizes coordinate vectors, as a list of lists
+    trials = []
+     
+    #For any number of simulations
+    for i in range(0,runs-1) :
+        trials[i][0] <- [x_0,y_0,t_0]
     
-      runsbysteps[j,,i]<-runsbysteps[j-1,,i]+c(dx,dy,tau)
-    }
-  }
-  
-  if (runs!=1)
-    {
-    results<-runsbysteps[steps,,]
-    } else{
-    results<-runsbysteps[,,1]
-    }
-  
-  return(results)
-}
+        #Computes position at each time step
+        for j in range(1,steps-1) :
+    
+            #theta is the direction of movement (from a uniform random distribution)
+            #0 is East
+            theta = r.uniform(0,2*n.pi)
+      
+            #displacement in this step
+            dx <- L * n.cos(theta)
+            dy <- L * n.sin(theta)
+    
+            trials[i][j] = map(add, trials[i][j-1],[dx, dy, tau])
+             
 
-SingleRun <- function(x_0, y_0, t_0, L, tau, steps, seed)
-{
-  coords <- RandomWalk(x_0, y_0, t_0, 1, 1, steps, 1, seed)
-  lim <- c(-1.25*max(abs(coords[,1:2])),1.25*max(abs(coords[,1:2])))
-  plot( coords[,1],coords[,2],type='l',bty='n',xaxt='n',yaxt='n',
-        xlab='x coordinate', ylab='y coordinate',
-        xlim=lim, ylim=lim)
-  axis(1, pos=0, col = "lightgrey")
-  axis(2, pos=0, col="lightgrey", las=1)
-}
+    if runs != 1 :
+        results = trials[:][-1]
+     
+    else :
+        results = trials[0]
+    
+    return(results)
 
-MultiRun <- function(x_0, y_0, t_0, L, tau, steps, runs, seed)
-{
-  results <- RandomWalk(x_0, y_0, t_0, L, tau, steps, runs, seed)
-  lim <- c(-1.25*max(abs(results[1:2,])),1.25*max(abs(results[1:2,])))
-  plot( results[1,],results[2,],type='p',bty='n',xaxt='n',yaxt='n',
-        xlab='x coordinate', ylab='y coordinate',
-        xlim=lim, ylim=lim)
-  axis(1, pos=0, col = "lightgrey")
-  axis(2, pos=0, col="lightgrey", las=1)
-  symbols(x_0, y_0, circles=5, add=T)
-}
-  
-MultiRun(0,0,0,1,1,100,100,666)
+Walk1 = RandomWalk(0,0,0,1,1,100,1,999)
+print(Walk1)
+
+#SingleRun <- function(x_0, y_0, t_0, L, tau, steps, seed)
+#{
+#  coords <- RandomWalk(x_0, y_0, t_0, 1, 1, steps, 1, seed)
+#  lim <- c(-1.25*max(abs(coords[,1:2])),1.25*max(abs(coords[,1:2])))
+#  plot( coords[,1],coords[,2],type='l',bty='n',xaxt='n',yaxt='n',
+#        xlab='x coordinate', ylab='y coordinate',
+#        xlim=lim, ylim=lim)
+#  axis(1, pos=0, col = "lightgrey")
+#  axis(2, pos=0, col="lightgrey", las=1)
+#}
+#
+#MultiRun <- function(x_0, y_0, t_0, L, tau, steps, runs, seed)
+#{
+#  results <- RandomWalk(x_0, y_0, t_0, L, tau, steps, runs, seed)
+#  lim <- c(-1.25*max(abs(results[1:2,])),1.25*max(abs(results[1:2,])))
+#  plot( results[1,],results[2,],type='p',bty='n',xaxt='n',yaxt='n',
+#        xlab='x coordinate', ylab='y coordinate',
+#        xlim=lim, ylim=lim)
+#  axis(1, pos=0, col = "lightgrey")
+#  axis(2, pos=0, col="lightgrey", las=1)
+#  symbols(x_0, y_0, circles=5, add=T)
+#}
+#  
+#MultiRun(0,0,0,1,1,100,100,666)
 
 
