00182f5d6bbbc3df97ae3270302760aa0209a293
chbrowning <13brownc@gmail.com>
2018-02-27 21:12:29 -0500

    function template

---
+++ func.py
@@ -0,0 +1,15 @@
+def function1(a, b, c):
+    # I return the result of adding a, b, and c together
+    x = a
+    y = b
+    z = c
+    result = x + y + z
+    return result
+
+def unpack_me(a, b, c):
+    # I return a tuple with 3 elements
+    x = a
+    y = b
+    z = c
+    return x, y, z
+
